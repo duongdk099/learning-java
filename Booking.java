b03d3c973e7ad7ae036e62693c237e0962ae6d87
duongdk099 <duongdk69@gmail.com>
2020-09-10 23:29:24 +0200

    Update Booking

---
+++ Booking.java
@@ -0,0 +1,14 @@
+public class Booking {
+    private String seat;
+
+    public String getSeat(){
+        return this.seat;
+    }
+    public getArrivalTime
+
+
+    public static void main (String [] args)
+    {
+
+    }
+}
