9a0c14db810506bb3fe32e5b3d96c90723d84796
Kihansi <ndh3395@gmail.com>
2020-09-23 23:49:44 +0200

    gitignore

--- Person.java
+++ Person.java
@@ -1,22 +1,29 @@
-public class Person {
-	private String name;
-	private int age;
-	private String adress;
+public abstract class Person {
 
-	public Person(String name, int age, String address)
-	{
-		this.name = name;
-		this.age = age;
-		this.adress = address;
-	}
+		/**
+		* class Person: model a person
+		*/
 
-	public String getName() {
-		return name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public boolean isOlder() {
-		return this.age>65;
-	}
+		private String name;
+		private int age;
+		private String adress;
+
+		public Person(String name, int age, String address)
+		{
+				this.name = name;
+				this.age = age;
+				this.adress = address;
+		}
+
+		public String getName() {
+				return name;
+		}
+		public int getAge() {
+				return age;
+		}
+		public boolean isOlder() {
+				return this.age > 65;
+		}
+
+
 }
